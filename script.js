aa2a33aa262d0b13a35d9abd9cf739511d7fc4f0
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2026-06-26 20:35:00 +0530

    module separated

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
+import { toggleHiddenElement } from "../modules/dom-functions.js";
+
 let buttonElement = document.getElementById('secret-button');
 let pElement = document.getElementById('p-element');
-
-function toggleHiddenElement(domElement){
-
-    if(domElement.style.display === "none"){
-        domElement.style.display = "block";
-    }
-    else{
-        domElement.style.display = "none";
-    }
-}
 
 buttonElement.addEventListener("click", ()=> toggleHiddenElement(pElement));
