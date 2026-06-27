bf189fbbf1ceff109362723397294fb6a45496cf
Abhishek Shrivastava <abhishekshrivastava78@gmail.com>
2026-06-27 13:35:07 +0530

    button color change implemented

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-import { toggleHiddenElement } from "../modules/dom-functions.js";
+import { toggleHiddenElement, changeToFunkyColor } from "../modules/dom-functions.js";
 
 let buttonElement = document.getElementById('secret-button');
 let pElement = document.getElementById('p-element');
 
-buttonElement.addEventListener("click", ()=> toggleHiddenElement(pElement));
+buttonElement.addEventListener("click", () => {
+    toggleHiddenElement(pElement);
+    changeToFunkyColor(buttonElement);
+});
